00b580435d581613c9914dab0130d3ab83275068
islamsamy214 <islamsamy214@gmail.com>
2023-07-19 12:13:11 +0300

    mentions apis

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\Api\BirthDateController;
 use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\MentionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,12 +27,18 @@ require __DIR__ . '/auth.php';
 
 Route::group(['as' => 'api.', 'middleware' => 'jwt:api'], function () {
     // profile apis
-    Route::put("user", [ProfileController::class, 'update'])->name('user.update');
-    Route::put("user/theme", [ProfileController::class, 'updateTheme'])->name('user.update-theme');
-    Route::put("user/experience", [ProfileController::class, 'updateExperience'])->name('user.update-experience');
-    Route::put("user/education", [ProfileController::class, 'updateEducation'])->name('user.update-education');
-    Route::put("user/certification", [ProfileController::class, 'updateCertification'])->name('user.update-certification');
-    Route::put("user/social", [ProfileController::class, 'updateSocial'])->name('user.update-social');
+    Route::put("profile", [ProfileController::class, 'update'])->name('user.update');
+    Route::put("profile/theme", [ProfileController::class, 'updateTheme'])->name('user.update-theme');
+    Route::put("profile/experience", [ProfileController::class, 'updateExperience'])->name('user.update-experience');
+    Route::put("profile/education", [ProfileController::class, 'updateEducation'])->name('user.update-education');
+    Route::put("profile/certification", [ProfileController::class, 'updateCertification'])->name('user.update-certification');
+    Route::put("profile/social", [ProfileController::class, 'updateSocial'])->name('user.update-social');
+
+    // user apis
+    Route::get("users", [UserController::class, 'index'])->name('users');
+
+    // mentions apis
+    Route::post("mentions", [MentionController::class, 'store'])->name('mentions.store');
 
     // search apis
     Route::post("search", SearchConroller::class)->name('search');
@@ -48,7 +56,6 @@ Route::group(['as' => 'api.', 'middleware' => 'jwt:api'], function () {
 
     // events apis
     Route::get("events", [EventController::class, 'getEvents'])->name('events');
-    // Route::resource('posts', PostController::class)->except(['show', 'create','store']);
 
     Route::resource("posts", PostController::class)->except(['create', 'edit']);
     Route::put("posts/{post}/like", [PostController::class, 'reactLike'])->name('posts.like');
