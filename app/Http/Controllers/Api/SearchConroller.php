fb68688269b94ca234e43239bde3f41e65b5c8ad
islamsamy214 <islamsamy214@gmail.com>
2023-07-26 11:32:28 +0300

    adj search api

--- app/Http/Controllers/Api/SearchConroller.php
+++ app/Http/Controllers/Api/SearchConroller.php
@@ -21,28 +21,12 @@ class SearchConroller extends Controller
         } //end of validate
 
         $search = $request->search;
-        $events = \App\Models\Event::search($search)->get();
         $users = \App\Models\User::search($search)->get();
         $posts = \App\Models\Post::search($search)->get();
-        $polls = \App\Models\Poll::search($search)->get();
-        $experiences = \App\Models\Experience::search($search)->get();
-        $companies = \App\Models\Company::search($search)->get();
-        $colleges = \App\Models\College::search($search)->get();
-        $departments = \App\Models\Department::search($search)->get();
-        $certifications = \App\Models\Certification::search($search)->get();
-        $educations = \App\Models\Education::search($search)->get();
 
         $response_data = [
-            'events' => $events,
             'users' => $users,
             'posts' => $posts,
-            'polls' => $polls,
-            'experiences' => $experiences,
-            'companies' => $companies,
-            'colleges' => $colleges,
-            'departments' => $departments,
-            'certifications' => $certifications,
-            'educations' => $educations,
         ]; //end of response_data
 
         $seo = Seo::first();
